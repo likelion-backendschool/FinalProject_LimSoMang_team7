3ce0fd4749e7501590b1b6af6a764b7db801effc
somang-lim <somangjuha@gmail.com>
2022-10-18 15:37:53 +0900

    FEAT: 1Week_Mission 추가

---
+++ 1Week_Mission/src/main/java/com/ll/exam/ebooks/EBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.ebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBooksApplication.class, args);
+    }
+
+}
---
+++ 1Week_Mission/src/test/java/com/ll/exam/ebooks/EBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.ebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
