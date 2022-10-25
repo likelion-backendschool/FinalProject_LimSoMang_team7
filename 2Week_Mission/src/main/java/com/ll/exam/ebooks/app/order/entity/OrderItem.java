2d3b0f3ce1d2654db7f0cfb8804df50da02826d7
somang-lim <somangjuha@gmail.com>
2022-10-25 14:39:35 +0900

    #13 - Feature: OrderItem Entity 생성

--- 2Week_Mission/src/main/java/com/ll/exam/ebooks/app/order/entity/OrderItem.java
+++ 2Week_Mission/src/main/java/com/ll/exam/ebooks/app/order/entity/OrderItem.java
@@ -1,7 +1,7 @@
 package com.ll.exam.ebooks.app.order.entity;
 
 import com.ll.exam.ebooks.app.base.entity.BaseEntity;
-import com.ll.exam.ebooks.app.member.entity.Member;
+import com.ll.exam.ebooks.app.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,24 +23,33 @@ import static javax.persistence.FetchType.LAZY;
 @NoArgsConstructor
 @SuperBuilder
 @ToString(callSuper = true)
-public class Order extends BaseEntity {
+public class OrderItem extends BaseEntity {
     @ManyToOne(fetch = LAZY)
-    private Member buyer;
+    @ToString.Exclude
+    private Order order;
+
+    @ManyToOne(fetch = LAZY)
+    private Product product;
 
     private LocalDateTime payDate; // 결제 날짜
 
-    private boolean readyStatus; // 주문 완료 여부
+    private int price; // 권장 판매가
 
-    private boolean isPaid; // 결제 완료 여부
+    private int salePrice; // 실제 판매가
 
-    private boolean isCanceled; // 취소 여부
+    private int wholesalePrice; // 도매가
 
-    private boolean isReturned; // 환불 여부
+    private int pgFee; // 결재대행사 수수료
 
-    private String name; // 주문명
+    private int payPrice; // 결제 금액
 
-    public Order(long id) {
+    private int refundPrice; // 환불 금액
+
+    private boolean isPaid; // 결제 여부
+
+    public OrderItem(long id) {
         super(id);
     }
+
 
 }
