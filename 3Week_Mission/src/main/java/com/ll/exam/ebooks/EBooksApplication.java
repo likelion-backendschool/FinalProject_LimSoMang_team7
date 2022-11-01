39e20b6f926fb6dee0a56adf78fa5b595d045961
somang-lim <somangjuha@gmail.com>
2022-11-01 21:19:18 +0900

    feat: @EnableScheduling 으로 Scheduling 기능 켜기 (#16)

--- 3Week_Mission/src/main/java/com/ll/exam/ebooks/EBooksApplication.java
+++ 3Week_Mission/src/main/java/com/ll/exam/ebooks/EBooksApplication.java
@@ -1,11 +1,15 @@
 package com.ll.exam.ebooks;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableBatchProcessing
+@EnableScheduling
 public class EBooksApplication {
 
     public static void main(String[] args) {
